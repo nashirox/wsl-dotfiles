f30652099577abb9f8b449a684290dba3284d593
Daisuke Nashiro <nashirox@gmail.com>
2022-10-13 12:34:20 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -40,4 +40,20 @@ brew bundle
 
 cd $HOME
 
+# install Docker
+if !(type docker > /dev/null 2>&1); then
+    sudo apt install -yq \
+        ca-certificates \
+        curl \
+        gnupg \
+        lsb-release
+   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+   echo \
+       "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+       $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt -qq update
+    sudo apt install -yq docker-ce docker-ce-cli containerd.io docker-compose-plugin
+    echo
+fi
+
 exec $SHELL
