35bc75a0ad791cba4a592b629fdba20e3def3f5c
Daisuke Nashiro <nashirox@gmail.com>
2022-10-13 12:55:18 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -58,6 +58,8 @@ fi
 
 # install latest Ruby
 if !(type ruby > /dev/null 2>&1); then
+    # https://github.com/rbenv/ruby-build/wiki#ubuntudebianmint
+    sudo apt install -yq autoconf bison patch build-essential rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev
     LATEST_RUBY_VERSION=$(rbenv install -l | grep -v - | tail -1)
     rbenv install $LATEST_RUBY_VERSION
     rbenv global $LATEST_RUBY_VERSION
